6b2f7a18d3482b62bc48db6f5a8d65e6a3576ddf
Radek Burget <burgetr@fit.vutbr.cz>
2016-01-28 23:02:30 +0100

    Annotator GUI completely reworked

--- src/main/java/org/fit/layout/annotator/AnnotatorPlugin.java
+++ src/main/java/org/fit/layout/annotator/AnnotatorPlugin.java
@@ -2,91 +2,100 @@ package org.fit.layout.annotator;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+
 import org.fit.layout.gui.AreaSelectionListener;
 import org.fit.layout.gui.Browser;
 import org.fit.layout.gui.BrowserPlugin;
-import org.fit.layout.impl.DefaultArea;
+import org.fit.layout.impl.DefaultTag;
 import org.fit.layout.model.Area;
 import org.fit.layout.model.Tag;
+
 import javax.swing.JComboBox;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
-import java.awt.Color;
+
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.util.Map;
+
+import javax.swing.JCheckBox;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 
 
 
 /**
- * Plugin enables area tree annotation
+ * A GUI plugin for manual annotation of visual areas by tags.
  * 
  * @author milicka
- *
+ * @author burgetr
  */
-public class AnnotatorPlugin implements BrowserPlugin {
+public class AnnotatorPlugin implements BrowserPlugin, AreaSelectionListener
+{
+	private Browser browser;
+    private String[] tags = new String[] {"h1","h2","h3","perex","paragraph","title","date","person" };
+    private Area selectedArea;
 
-	Browser browser;
 	private JPanel pnl_mainPanel;
 	private JPanel pnl_selection;
 	private JComboBox<String> cbx_tagSelector;
 	private JPanel pnl_control;
-	private JButton btn_toogleEnableAnotation;
-	private JButton btn_clearAnotation;
-	private AnnotatorAreaListener listener;
+	private JButton btn_addTag;
+	private JButton btn_removeTag;
 	private JScrollPane scrl_tagTable;
 	private JTable tagTable;
-	private DefaultTableModel tagModelTable;
-	
-	
-	private Boolean enableAnotation = false;
-	private String[] tags = new String[] {"h1","h2","h3","perex","paragraph","title","date","person" };
+	private DefaultTableModel tagTableModel;
+	private JPanel pnl_settings;
+	private JCheckBox chckbxHighlightTags;
+	private JPanel pnl_info;
+	private JTextField txtType;
 	
 	/**
 	 * @wbp.parser.entryPoint
 	 */
 	@Override
-	public boolean init(Browser browser) {
+	public boolean init(Browser browser) 
+	{
 		this.browser = browser;
-		
-		this.browser.addStructurePanel("Annotator", getPnl_mainPanel());
-		
-		this.browser.addAreaSelectionListener(getListener_areaSelection());
+		this.browser.addToolPanel("Annotator", getPnl_mainPanel());
+		this.browser.addAreaSelectionListener(this);
 		return true;
 	}
 
-	private AreaSelectionListener getListener_areaSelection () {
-		
-		if(listener == null) {
-			listener = new AnnotatorAreaListener() { 
-				@Override
-				public void areaSelected(Area area) {
-					super.areaSelected(area);
-					
-					if(!enableAnotation) {
-						return;
-					}
-						
-					String actualTag = getCbx_tagSelection().getSelectedItem().toString();
-					AnnotatorTag aTag = new AnnotatorTag(actualTag);
-					
-					if( area.hasTag(aTag) ) {
-						removeTag((DefaultArea)area, aTag);
-					}
-					else {
-						addTag(area, aTag);
-					}
-				}
-			};
-		}
-		
-		return listener;
-	}
-	
+    @Override
+    public void areaSelected(Area area)
+    {
+        selectedArea = area;
+        updateTableModel();
+        getBtn_addTag().setEnabled(area != null);
+    }
+
+    private void updateTableModel()
+    {
+        //remove all rows
+        tagTableModel.setRowCount(0);
+        //add the tag rows
+        if (selectedArea != null)
+        {
+            for (Map.Entry<Tag, Float> tagEntry : selectedArea.getTags().entrySet())
+            {
+                Tag tag = tagEntry.getKey();
+                Float support = tagEntry.getValue();
+                tagTableModel.addRow(new Object[] { tag.getType(), tag.getValue(), support });
+            }
+        }
+    }
+    
+    //===========================================================================
+    
     private JPanel getPnl_mainPanel()
     {
         if (pnl_mainPanel == null)
@@ -94,45 +103,72 @@ public class AnnotatorPlugin implements BrowserPlugin {
             pnl_mainPanel = new JPanel();
             
             GridBagLayout gbl_pathsPanel = new GridBagLayout();
-            gbl_pathsPanel.columnWidths = new int[] { 0, 0 };
-            gbl_pathsPanel.rowHeights = new int[] { 0, 0, 0 };
-            gbl_pathsPanel.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
-            gbl_pathsPanel.rowWeights = new double[] { 0.05, 0.05, 1.0,
-                    Double.MIN_VALUE };
+            gbl_pathsPanel.columnWeights = new double[] { 0.0, 1.0, 1.0 };
+            gbl_pathsPanel.rowWeights = new double[] { 0.05, 0.05, 1.0 };
             pnl_mainPanel.setLayout(gbl_pathsPanel);
             
             GridBagConstraints gbc_selection = new GridBagConstraints();
-            gbc_selection.insets = new Insets(0, 0, 5, 0);
-            gbc_selection.fill = GridBagConstraints.BOTH;
+            gbc_selection.insets = new Insets(0, 0, 5, 5);
+            gbc_selection.fill = GridBagConstraints.HORIZONTAL;
             gbc_selection.gridx = 0;
             gbc_selection.gridy = 0;
             pnl_mainPanel.add(getPnl_selection(), gbc_selection);
             
             GridBagConstraints gbc_control = new GridBagConstraints();
+            gbc_control.insets = new Insets(0, 0, 5, 5);
             gbc_control.fill = GridBagConstraints.HORIZONTAL;
             gbc_control.gridx = 0;
             gbc_control.gridy = 1;
             pnl_mainPanel.add(getPnl_control(), gbc_control);
             
             GridBagConstraints gbc_extractionScroll = new GridBagConstraints();
+            gbc_extractionScroll.insets = new Insets(0, 0, 5, 0);
+            gbc_extractionScroll.weighty = 1.0;
+            gbc_extractionScroll.gridheight = 3;
             gbc_extractionScroll.fill = GridBagConstraints.BOTH;
-            gbc_extractionScroll.gridx = 0;
-            gbc_extractionScroll.gridy = 2;
+            gbc_extractionScroll.gridx = 1;
+            gbc_extractionScroll.gridy = 0;
             pnl_mainPanel.add(getScrl_tagTable(), gbc_extractionScroll);
+            GridBagConstraints gbc_pnl_settings = new GridBagConstraints();
+            gbc_pnl_settings.insets = new Insets(0, 0, 5, 5);
+            gbc_pnl_settings.fill = GridBagConstraints.BOTH;
+            gbc_pnl_settings.gridx = 0;
+            gbc_pnl_settings.gridy = 2;
+            pnl_mainPanel.add(getPnl_settings(), gbc_pnl_settings);
+            GridBagConstraints gbc_pnl_info = new GridBagConstraints();
+            gbc_pnl_info.weighty = 1.0;
+            gbc_pnl_info.weightx = 1.0;
+            gbc_pnl_info.gridheight = 3;
+            gbc_pnl_info.insets = new Insets(0, 0, 0, 5);
+            gbc_pnl_info.fill = GridBagConstraints.BOTH;
+            gbc_pnl_info.gridx = 2;
+            gbc_pnl_info.gridy = 0;
+            pnl_mainPanel.add(getPanel_1(), gbc_pnl_info);
             
             
         }
         return pnl_mainPanel;
     }
 
-    
-    
-    //selection panel ====================
     private JPanel getPnl_selection() {
     	if(pnl_selection==null) {
     		pnl_selection = new JPanel();
-    		pnl_selection.setLayout( new FlowLayout(FlowLayout.CENTER) );
-    		pnl_selection.add(getCbx_tagSelection());
+    		GridBagLayout gbl_pnl_selection = new GridBagLayout();
+    		gbl_pnl_selection.columnWeights = new double[]{1.0};
+    		gbl_pnl_selection.rowWeights = new double[]{0.0, 0.0};
+    		pnl_selection.setLayout(gbl_pnl_selection);
+    		GridBagConstraints gbc_txtType = new GridBagConstraints();
+    		gbc_txtType.fill = GridBagConstraints.BOTH;
+    		gbc_txtType.insets = new Insets(5, 5, 0, 5);
+    		gbc_txtType.gridx = 0;
+    		gbc_txtType.gridy = 0;
+    		pnl_selection.add(getTxtType(), gbc_txtType);
+    		GridBagConstraints gbc_cbx_tagSelector = new GridBagConstraints();
+    		gbc_cbx_tagSelector.insets = new Insets(0, 5, 0, 5);
+    		gbc_cbx_tagSelector.fill = GridBagConstraints.HORIZONTAL;
+    		gbc_cbx_tagSelector.gridx = 0;
+    		gbc_cbx_tagSelector.gridy = 1;
+    		pnl_selection.add(getCbx_tagSelection(), gbc_cbx_tagSelector);
     	}
     	return pnl_selection;
     }
@@ -148,66 +184,60 @@ public class AnnotatorPlugin implements BrowserPlugin {
 		return cbx_tagSelector;
 	}
 	
-	
-	
-	//control panel ====================
     private JPanel getPnl_control() {
     	if(pnl_control==null) {
     		pnl_control = new JPanel();
     		pnl_control.setLayout( new FlowLayout(FlowLayout.CENTER) );
-    		pnl_control.add(getBtn_toogleEnableAnotation() );
-    		pnl_control.add(getBtn_clearAnotation());
+    		pnl_control.add(getBtn_addTag() );
+    		pnl_control.add(getBtn_removeTag());
     	}
     	return pnl_control;
     }
 
-	private JButton getBtn_toogleEnableAnotation() {
-		if (btn_toogleEnableAnotation == null) {
-			enableAnotation = false;
+	private JButton getBtn_addTag() {
+		if (btn_addTag == null) {
 			
-			btn_toogleEnableAnotation = new JButton("Enable anotation");
-			btn_toogleEnableAnotation.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent arg0) {
-					if(!enableAnotation) {
-						enableAnotation = true;
-						btn_toogleEnableAnotation.setText("Disable anotation");
-						tagTable.setBackground(Color.white);
-					}
-					else {
-						enableAnotation = false;
-						btn_toogleEnableAnotation.setText("Enable anotation");
-						tagTable.setBackground(Color.lightGray);
-					}
+			btn_addTag = new JButton("Assign");
+			btn_addTag.setEnabled(false);
+			btn_addTag.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) 
+				{
+				    if (selectedArea != null)
+				    {
+				        Tag tag = new DefaultTag(txtType.getText(), cbx_tagSelector.getSelectedItem().toString());
+				        selectedArea.addTag(tag, 1.0f);
+				        updateTableModel();
+				    }
 				}
 			});
 		}
-		return btn_toogleEnableAnotation;
+		return btn_addTag;
 	}
 
-	private JButton getBtn_clearAnotation() {
-		if (btn_clearAnotation == null) {
+	private JButton getBtn_removeTag() {
+		if (btn_removeTag == null) {
 			
-			btn_clearAnotation = new JButton("Clear");
-			btn_clearAnotation.addActionListener(new ActionListener() 
+			btn_removeTag = new JButton("Remove");
+			btn_removeTag.setEnabled(false);
+			btn_removeTag.addActionListener(new ActionListener() 
 			{
 				public void actionPerformed(ActionEvent arg0) 
 				{
-					for(int i=tagModelTable.getRowCount(); i>0; i--) 
-					{
-						String tag = (String)tagModelTable.getValueAt(i-1, 0);
-						DefaultArea area = (DefaultArea)tagModelTable.getValueAt(i-1, 1);
-						removeTag(area, new AnnotatorTag(tag));
-					}
-						
+				    int row = tagTable.getSelectedRow();
+				    if (row != -1)
+				    {
+				        Tag tag = new DefaultTag(tagTableModel.getValueAt(row, 0).toString(),
+				                tagTableModel.getValueAt(row, 1).toString());
+				        if (selectedArea != null)
+				            selectedArea.removeTag(tag);
+				        updateTableModel();
+				    }
 				}
 			});
 		}
-		return btn_clearAnotation;
+		return btn_removeTag;
 	}
 
-	
-	
-	//anotation list ========================
 	private JScrollPane getScrl_tagTable()
     {
         if (scrl_tagTable == null)
@@ -222,51 +252,63 @@ public class AnnotatorPlugin implements BrowserPlugin {
     {
         if (tagTable == null)
         {
-        	tagModelTable = new DefaultTableModel(); 
-        	tagModelTable.addColumn("Tag"); 
-        	tagModelTable.addColumn("Content");
+        	tagTableModel = new DefaultTableModel() {
+                private static final long serialVersionUID = 1L;
+                public boolean isCellEditable(int row, int column) 
+                {
+                    return false;
+                };
+            };
+            tagTableModel.addColumn("Type"); 
+        	tagTableModel.addColumn("Name"); 
+        	tagTableModel.addColumn("Support");
         	
-            tagTable = new JTable(tagModelTable);
+            tagTable = new JTable(tagTableModel);
+            tagTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+            tagTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+                @Override
+                public void valueChanged(ListSelectionEvent ev)
+                {
+                    getBtn_removeTag().setEnabled(tagTable.getSelectedRow() != -1);
+                }
+            });
         }
         return tagTable;
     }
     
+    private JPanel getPnl_settings() 
+    {
+        if (pnl_settings == null) {
+        	pnl_settings = new JPanel();
+        	pnl_settings.add(getChckbxHighlightTags());
+        }
+        return pnl_settings;
+    }
     
+    private JCheckBox getChckbxHighlightTags() 
+    {
+        if (chckbxHighlightTags == null) {
+        	chckbxHighlightTags = new JCheckBox("Highlight tags");
+        }
+        return chckbxHighlightTags;
+    }
     
-    //operation function ===========================
-	
-	/**
-	 * Adds tag to GUI table and area object
-	 * @param area
-	 * @param tag
-	 */
-	private void addTag(Area area, Tag tag) 
-	{
-		area.addTag(tag, 1.0f);
-		tagModelTable.addRow(new Object[] { (getCbx_tagSelection().getSelectedItem()).toString(), area });
-	}
-	
-	/**
-	 * Does complete removing from GUI table and area object
-	 * @param area
-	 * @param tag
-	 */
-	private void removeTag(DefaultArea area, AnnotatorTag tag) 
-	{
-		//Removes tag from the specific area object
-		if (area.containsTag(tag)) {
-			area.removeTag(tag);
-		}
+    private JPanel getPanel_1() 
+    {
+        if (pnl_info == null) {
+        	pnl_info = new JPanel();
+        }
+        return pnl_info;
+    }
+    
+    private JTextField getTxtType() 
+    {
+        if (txtType == null) {
+        	txtType = new JTextField();
+        	txtType.setText("FitLayout.Annotate");
+        	txtType.setColumns(10);
+        }
+        return txtType;
+    }
 
-		//Removes table row for defined area and tag
-		for( int i=0; i<tagModelTable.getRowCount(); i++ ) 
-		{
-			Area aa = (Area)tagModelTable.getValueAt(i, 1);
-			String tagString = (String)tagModelTable.getValueAt(i, 0);
-	
-			if(area.equals(aa) && tag.equals( new AnnotatorTag(tagString) )) {
-				tagModelTable.removeRow(i);
-			}
-		}
-	}
 }
